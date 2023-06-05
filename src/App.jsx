35700ba846b59e5b7b580504605767b5b5a0fe77
MONTOYA, Alan <mon21033@byui.edu>
2023-06-04 23:36:36 -0500

    Componentes y manejo de props / Funciones y Clases

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Greeting from './components/pure/greeting'
+import Greetingf from './components/pure/greetingF'
 import TaskListComponent from './components/containers/task_list'
 
 function App() {
@@ -9,6 +11,10 @@ function App() {
 
   return (
     <>
+    {/* Componente propio Greeting.jsx */}
+    {/* <Greeting name={"John Connor"}/> */}
+    {/* Componente funcional Greetingf.jsx */}
+    {/* <Greetingf name={"John Connor"}/> */}
     {/* Task list component */}
     <TaskListComponent/>
     </>
